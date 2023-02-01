9bd908e569be7ed774d8bd9ffc140b6260aa1f64
BEN OKELLO MWAKA <mwakaben@gmail.com>
2023-02-02 02:10:08 +0300

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head> 
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-with, initial-scale=1.0">
+        <meta http-equiv="X-UA-Coompatible" content="ie=edge">
+        <title>Post</title>
+    </head>
+    <body> 
+        @yield('content')
+    </body>
+</html>
